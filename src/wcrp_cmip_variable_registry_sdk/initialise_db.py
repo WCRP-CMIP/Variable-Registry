915de8d0c0be344dc0563c6980de3fe3bf7d9b4d
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-09-13 09:41:17 +0200

    Add better handling of failures

--- src/wcrp_cmip_variable_registry_sdk/initialise_db.py
+++ src/wcrp_cmip_variable_registry_sdk/initialise_db.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 from pathlib import Path
 
 import sqlalchemy.engine.base
-from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel import Session, SQLModel, create_engine, text
 
 from wcrp_cmip_variable_registry_sdk.db import DBInfo
 
@@ -38,6 +38,10 @@ def initialise_sqlite_database_from_source_root(
         sqlite_url,
         # echo=True,
     )
+    # Ensure that trying to add data which points to foreign keys
+    # that don't exist will fail
+    with engine.connect() as connection:
+        connection.execute(text("PRAGMA foreign_keys=ON"))
 
     # Make sure that the needed classes are registered by importing them
     from wcrp_cmip_variable_registry_sdk.db.models import (
@@ -52,11 +56,30 @@ def initialise_sqlite_database_from_source_root(
     with Session(engine) as session:
         db_info = DBInfo.from_source_root(source_root)
 
+        tmp = list(db_info.variables_by_id.values())
+        tmp_safe = []
+        for v in tmp:
+            if (
+                v.variable_root_id in db_info.variable_roots_by_id
+                and v.horizontal_label_id in db_info.horizontal_labels_by_id
+            ):
+                tmp_safe.append(v)
+            else:
+                print(f"Missing references for {v.validation_key}")
+
+        print("")
+        print(f"{len(tmp_safe)} variables will be included in the DB")
+        print(
+            f"{len(tmp) - len(tmp_safe)} variables have issues "
+            "and will not be included in the DB"
+        )
+        print("")
+
         session.add_all(
             (
                 *db_info.horizontal_labels_by_id.values(),
-                *db_info.variables_by_id.values(),
                 *db_info.variable_roots_by_id.values(),
+                *tmp_safe,
             )
         )
 
